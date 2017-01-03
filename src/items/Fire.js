8abdb0c430495f093bb8f38ed89c073a84af1249
Tom Longson (nym) <tomlong@gmail.com>
2017-01-02 16:24:38 -0800

    Adding Fire item.

---
+++ src/items/Fire.js
@@ -0,0 +1,22 @@
+import Item from '../Item';
+
+class Fire extends Item {	
+	constructor(name) {
+    	super("Fire")
+	}
+
+	created() {
+		console.log("It's a fire. You can burn wood in it.");
+	}
+
+	destroy() {
+		console.log("Scattered the fire. It's just hot ash now.")
+	}
+
+	light() {
+		// requires a flint
+	}
+}
+Fire.cost = {"Wood": 10}
+
+export default Fire;
--- src/items/index.js
+++ src/items/index.js
@@ -2,6 +2,7 @@ export {default as Wood} from './Wood'
 export {default as Stone} from './Stone'
 export {default as Flint} from './Flint'
 
+export {default as Fire} from './Fire'
 export {default as Charcoal} from './Charcoal'
 export {default as Gunpowder} from './Gunpowder'
 export {default as Sulfur} from './Sulfur'
